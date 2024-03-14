ca40ed90c718af76953b089629c19054a1ef1482
achasssss <114160689+achasssss@users.noreply.github.com>
2024-03-14 14:32:58 +0530

    Update cloning.py

--- cloning.py
+++ cloning.py
@@ -1,10 +1,38 @@
+# import os
+# import streamlit as st
+# import torch
+# from TTS.api import TTS
+# from upload_process import get_vocal_audio
+# from config import LANGUAGES, OUTPUTS_DIR
+
+# def generate_cloned_voice(text, target_language, audio_file_path):
+#     st.write("Generating cloned voice...")
+
+#     output_file = os.path.join(OUTPUTS_DIR, "cloned_voice.wav")
+
+#     device = "cuda" if torch.cuda.is_available() else "cpu"
+
+#     tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+#     try:
+#         vocal_audio_path = get_vocal_audio(audio_file_path)
+
+#         tts.tts_to_file(text=text, speaker_wav=vocal_audio_path, language=LANGUAGES[target_language],
+#                         file_path=output_file)
+
+#         st.write("Cloned voice generated successfully!")
+#         st.audio(output_file)
+
+#     except Exception as e:
+#         st.error(f"Error generating cloned voice: {e}")
+
+
 import os
 import streamlit as st
 import torch
 from TTS.api import TTS
 from upload_process import get_vocal_audio
 from config import LANGUAGES, OUTPUTS_DIR
-import subprocess
 
 def generate_cloned_voice(text, target_language, audio_file_path):
     st.write("Generating cloned voice...")
@@ -13,8 +41,8 @@ def generate_cloned_voice(text, target_language, audio_file_path):
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    # Agree to the terms of service
-    subprocess.run(['echo', 'y', '|', 'python', '-m', 'TTS.api'])
+    # Skip terms of service check
+    os.environ['TTS_SKIP_TERMS_CHECK'] = '1'
 
     tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
