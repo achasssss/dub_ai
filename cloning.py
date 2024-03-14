100c861ab669f9d5ae79dc3b58ed7ef13778cc27
achasssss <114160689+achasssss@users.noreply.github.com>
2024-03-14 14:30:22 +0530

    Update cloning.py

--- cloning.py
+++ cloning.py
@@ -1,41 +1,10 @@
-# import os
-# import streamlit as st
-# import torch
-# from TTS.api import TTS
-# from upload_process import get_vocal_audio
-# from config import LANGUAGES, OUTPUTS_DIR
-
-
-# def generate_cloned_voice(text, target_language, audio_file_path):
-#     st.write("Generating cloned voice...")
-
-#     output_file = os.path.join(OUTPUTS_DIR, "cloned_voice.wav")
-
-#     device = "cuda" if torch.cuda.is_available() else "cpu"
-
-#     tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
-
-#     try:
-#         vocal_audio_path = get_vocal_audio(audio_file_path)
-
-#         tts.tts_to_file(text=text, speaker_wav=vocal_audio_path, language=LANGUAGES[target_language],
-#                         file_path=output_file)
-
-#         st.write("Cloned voice generated successfully!")
-#         st.audio(output_file)
-
-#     except Exception as e:
-#         st.error(f"Error generating cloned voice: {e}")
-
 import os
 import streamlit as st
 import torch
 from TTS.api import TTS
 from upload_process import get_vocal_audio
 from config import LANGUAGES, OUTPUTS_DIR
-
-def agree_to_terms_of_service():
-    return True  # Always return True to bypass user input
+import subprocess
 
 def generate_cloned_voice(text, target_language, audio_file_path):
     st.write("Generating cloned voice...")
@@ -44,19 +13,19 @@ def generate_cloned_voice(text, target_language, audio_file_path):
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    if agree_to_terms_of_service():
-        tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+    # Agree to the terms of service
+    subprocess.run(['echo', 'y', '|', 'python', '-m', 'TTS.api'])
 
-        try:
-            vocal_audio_path = get_vocal_audio(audio_file_path)
+    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
-            tts.tts_to_file(text=text, speaker_wav=vocal_audio_path, language=LANGUAGES[target_language],
-                            file_path=output_file)
+    try:
+        vocal_audio_path = get_vocal_audio(audio_file_path)
 
-            st.write("Cloned voice generated successfully!")
-            st.audio(output_file)
+        tts.tts_to_file(text=text, speaker_wav=vocal_audio_path, language=LANGUAGES[target_language],
+                        file_path=output_file)
 
-        except Exception as e:
-            st.error(f"Error generating cloned voice: {e}")
-    else:
-        st.error("You must agree to the terms of service to use this model.")
+        st.write("Cloned voice generated successfully!")
+        st.audio(output_file)
+
+    except Exception as e:
+        st.error(f"Error generating cloned voice: {e}")
